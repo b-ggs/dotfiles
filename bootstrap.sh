5ab1ede01e86d7e0af5a25ebb915fb496fdc0371
Boggs <hello.boggs@gmail.com>
2016-03-25 03:25:34 +0800

    Remove dot from filenames
    Add custom gnzh zsh theme

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
 
 dir=$PWD
-
 installdir=~
-files=".vimrc .ideavimrc .vim .zshrc .tmux.conf .dircolors"
+files="vimrc ideavimrc vim zshrc tmux.conf dircolors"
 for file in $files; do
     rm -rf $installdir/$file
     echo "Linking $dir/$file to $installdir/$file."
-    ln -s $dir/$file $installdir/$file
+    ln -s $dir/$file $installdir/.$file -f
 done
 
-# installdir=~/.config
-# files=""
-# for file in $files; do
-#     rm -rf $installdir/$file
-#     echo "Linking $dir/$file to $installdir/$file."
-#     ln -s $dir/$file $installdir/$file
-# done
+dir=$PWD/zsh
+installdir=~/.oh-my-zsh/custom/themes
+files="gnzh.zsh-theme"
+mkdir -p $installdir
+for file in $files; do
+    rm -rf $installdir/$file
+    echo "Linking $dir/$file to $installdir/$file."
+    ln -s $dir/$file $installdir/$file -f
+done

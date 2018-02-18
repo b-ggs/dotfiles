370bd4845858b6c7818c885f70ec810cfe109062
boggs <hello@boggs.xyz>
2018-02-19 05:26:07 +0800

    Move all Slate operations into one hash

--- slate/slate.js
+++ slate/slate.js
@@ -2,27 +2,39 @@
 // Edit configs here
 // =================
 
-var moveModifier = "cmd,alt";
-var moveKeys = {
-  "fullScreen": "f",
-  "fullScreenNoGap": "g",
-  "pushRight": "l",
-  "pushLeft": "h",
-  "pushUp": "k",
-  "pushDown": "j"
-};
-
-var focusModifier = "cmd,ctrl";
-var focusKeys = {
-  "focusRight": "l",
-  "focusLeft": "h",
-  "focusUp": "k",
-  "focusDown": "j"
-};
-
-var miscModifier = "cmd,ctrl";
-var miscKeys = {
-  "hint": "f"
+var bindings = {
+  "move": {
+    "modifier": "cmd,alt",
+    "keys": {
+      "fullScreen": "f",
+      "fullScreenNoGap": "g",
+      "pushRight": "l",
+      "pushLeft": "h",
+      "pushUp": "k",
+      "pushDown": "j"
+    }
+  },
+  "focus": {
+    "modifier": "cmd,ctrl",
+    "keys": {
+      "focusRight": "l",
+      "focusLeft": "h",
+      "focusUp": "k",
+      "focusDown": "j"
+    }
+  },
+  "hint": {
+    "modifier": "cmd,ctrl",
+    "keys": {
+      "hint": "f"
+    }
+  },
+  "grid": {
+    "modifier": "cmd,ctrl",
+    "keys": {
+      "grid": "g"
+    }
+  }
 };
 
 var gap = 10;
@@ -35,120 +47,135 @@ function iTermXOffset(win) {
   return win.app().name() == "iTerm2" ? "+ 2" : "";
 }
 
-function moveKeystroke(action) {
-  return moveKeys[action] + ':' + moveModifier;
+function keystroke(operation, action) {
+  var operationHash = bindings[operation]
+  var modifier = operationHash["modifier"]
+  var key = operationHash["keys"][action]
+  return key + ":" + modifier;
 }
 
 // Gaps calculation forked and modified from
 // https://gist.github.com/leb2/5af57cd4b011937dc6e0/
 
-function moveActionParams(action, win) {
-  moveParamsHash = {
-    "fullScreen": {
-      "x": "screenOriginX + " + gap + iTermXOffset(win),
-      "y": "screenOriginY + " + gap,
-      "width": "screenSizeX - " + (gap * 2),
-      "height": "screenSizeY - " + (gap * 2)
+function operationParams(operation, action, win) {
+  paramsHash = {
+    "move": {
+      "fullScreen": {
+        "x": "screenOriginX + " + gap + iTermXOffset(win),
+        "y": "screenOriginY + " + gap,
+        "width": "screenSizeX - " + (gap * 2),
+        "height": "screenSizeY - " + (gap * 2)
+      },
+      "fullScreenNoGap": {
+        "x": "screenOriginX",
+        "y": "screenOriginY",
+        "width": "screenSizeX",
+        "height": "screenSizeY"
+      },
+      "pushRight": {
+        "x": "screenSizeX / 2 + " + (gap / 2) + iTermXOffset(win),
+        "y": "screenOriginY + " + gap,
+        "width": "screenSizeX / 2 - " + (gap * 3 / 2),
+        "height": "screenSizeY - " + (gap * 2)
+      },
+      "pushLeft": {
+        "x": "screenOriginX + " + gap + iTermXOffset(win),
+        "y": "screenOriginY + " + gap,
+        "width": "screenSizeX / 2 - " + (gap * 3 / 2),
+        "height": "screenSizeY - " + (gap * 2)
+      },
+      "pushDown": {
+        "x": "windowTopLeftX",
+        "y": "screenSizeY / 2 + " + (gap / 2),
+        "height": "screenSizeY / 2 -" + (3 * gap / 2),
+        "width": "windowSizeX"
+      },
+      "pushUp": {
+        "x": "windowTopLeftX",
+        "y": gap,
+        "height": "screenSizeY / 2 -" + (3 * gap / 2),
+        "width": "windowSizeX"
+      },
     },
-    "fullScreenNoGap": {
-      "x": "screenOriginX",
-      "y": "screenOriginY",
-      "width": "screenSizeX",
-      "height": "screenSizeY"
+    "focus": {
+      "focusRight": {
+        "direction": "right"
+      },
+      "focusLeft": {
+        "direction": "left"
+      },
+      "focusUp": {
+        "direction": "up"
+      },
+      "focusDown": {
+        "direction": "down"
+      }
     },
-    "pushRight": {
-      "x": "screenSizeX / 2 + " + (gap / 2) + iTermXOffset(win),
-      "y": "screenOriginY + " + gap,
-      "width": "screenSizeX / 2 - " + (gap * 3 / 2),
-      "height": "screenSizeY - " + (gap * 2)
-    },
-    "pushLeft": {
-      "x": "screenOriginX + " + gap + iTermXOffset(win),
-      "y": "screenOriginY + " + gap,
-      "width": "screenSizeX / 2 - " + (gap * 3 / 2),
-      "height": "screenSizeY - " + (gap * 2)
-    },
-    "pushDown": {
-      "x": "windowTopLeftX",
-      "y": "screenSizeY / 2 + " + (gap / 2),
-      "height": "screenSizeY / 2 -" + (3 * gap / 2),
-      "width": "windowSizeX"
-    },
-    "pushUp": {
-      "x": "windowTopLeftX",
-      "y": gap,
-      "height": "screenSizeY / 2 -" + (3 * gap / 2),
-      "width": "windowSizeX"
-    },
-  };
-  return moveParamsHash[action];
-}
-
-function moveOperation(win, action) {
-  var operation = S.op("move", moveActionParams(action, win));
-  win.doOperation(operation);
-}
-
-var moveActions = [
-  "fullScreen",
-  "fullScreenNoGap",
-  "pushRight",
-  "pushLeft",
-  "pushUp",
-  "pushDown"
-];
-
-_.each(moveActions, function(moveAction) {
-  slate.bind(moveKeystroke(moveAction), function(win) { moveOperation(win, moveAction); });
-}, this);
-
-function focusKeystroke(action) {
-  return focusKeys[action] + ':' + focusModifier;
-}
-
-var focusActionParams = {
-  "focusRight": {
-    "direction": "right"
-  },
-  "focusLeft": {
-    "direction": "left"
-  },
-  "focusUp": {
-    "direction": "up"
-  },
-  "focusDown": {
-    "direction": "down"
-  }
-};
-
-function focusOperation(win, action) {
-  var operation = S.op("focus", focusActionParams[action]);
-  win.doOperation(operation);
-}
-
-var focusActions = [
-  "focusRight",
-  "focusLeft",
-  "focusUp",
-  "focusDown"
-];
-
-_.each(focusActions, function(focusAction) {
-  slate.bind(focusKeystroke(focusAction), function(win) { focusOperation(win, focusAction); });
-}, this);
-
-slate.bind("f:cmd,shift", function(win) {
-    win.doOperation(S.op("hint", {
+    "hint": {
+      "hint": {
         "characters": "asdfghjkl"
-    }));
-});
-
-slate.bind("1:ctrl", S.op("grid", {
-  "grids" : {
-    "1680x1050": {
-      "width": 16,
-      "height": 9
+      }
+    },
+    "grid": {
+      "grid": {
+        "grids": {
+          "1680x1050": {
+            "width": 16,
+            "height": 9
+          }
+        },
+        "padding": 5
+      }
     }
+  };
+  return paramsHash[operation][action];
+}
+
+function doOperation(win, operation, action) {
+  var operation = S.op(operation, operationParams(operation, action, win));
+  win.doOperation(operation);
+}
+
+var operationActionHashes = [
+  {
+    "operation": "move",
+    "actions": [
+      "fullScreen",
+      "fullScreenNoGap",
+      "pushRight",
+      "pushLeft",
+      "pushUp",
+      "pushDown"
+    ]
   },
-  "padding": 5
-}));
+  {
+    "operation": "focus",
+    "actions": [
+      "focusRight",
+      "focusLeft",
+      "focusUp",
+      "focusDown"
+    ]
+  },
+  {
+    "operation": "hint",
+    "actions": [
+      "hint"
+    ]
+  },
+  {
+    "operation": "grid",
+    "actions": [
+      "grid"
+    ]
+  }
+];
+
+_.each(operationActionHashes, function(operationActionHash) {
+  var operation = operationActionHash["operation"];
+  _.each(operationActionHash["actions"], function(action) {
+    S.bind(keystroke(operation, action), function(win) {
+      doOperation(win, operation, action);
+    });
+  }, this);
+}, this);

6db79f711cb4d100241b4b9698ae8983a22e97e3
boggs <hello@boggs.xyz>
2018-02-20 23:44:47 +0800

    [Scripts] - Return Spotify output in JSON for easier parsing in apps that use the script

--- scripts/spotify_macos.sh
+++ scripts/spotify_macos.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
-darwin_check=`uname -s`
-if [[ $darwin_check != "Darwin" ]]; then
-  echo "System is unsupported"
-  exit 1
-fi
 spotify_check=`ps aux | grep /Applications/Spotify.app/Contents/MacOS/Spotify | wc -l | awk '{$1=$1};1'`
 if [[ $spotify_check == "1" ]]; then
-  echo "Spotify is not running"
+  echo '{"state": "not running"}'
   exit 1
 fi
+
+state=`osascript -e 'tell application "Spotify" to player state as string'`
 title=`osascript -e 'tell application "Spotify" to name of current track as string'`
 artist=`osascript -e 'tell application "Spotify" to artist of current track as string'`
-resp="${title}@${artist}"
-status=`osascript -e 'tell application "Spotify" to player state as string'`
-if [[ $status == "paused" ]]; then
-  resp="${resp}@paused"
-fi
+
+resp='{ '
+resp="${resp}\"state\": \"${state}\", "
+resp="${resp}\"title\": \"${title}\", "
+resp="${resp}\"artist\": \"${artist}\""
+resp="${resp} }"
+
 echo $resp

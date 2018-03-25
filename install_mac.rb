b2f391a100bf26925c999df4c759c3558c538a0c
boggs <hello@boggs.xyz>
2018-03-26 05:04:35 +0800

    install_mac - Add methods to install cask dependencies from homebrew-cask

--- install_mac.rb
+++ install_mac.rb
@@ -4,6 +4,7 @@ def run
   init
   install_homebrew
   install_dependencies
+  install_cask_dependencies
   set_zsh_default
   install_oh_my_zsh
   link_dotfiles
@@ -22,7 +23,6 @@ def install_homebrew
   if type('brew')[:success?]
     log 'Homebrew already exists. Skipping...'
   else
-    log 'Installing Homebrew...'
     if shell('/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"')[:success?]
       log 'Successfully installed Homebrew.'
     else
@@ -33,13 +33,30 @@ def install_homebrew
 end
 
 def install_dependencies
-  log "Installing dependencies..."
+  log "Installing dependencies from Homebrew..."
   dependencies.each do |dependency|
     log "Installing #{dependency}..."
     if shell("brew list #{dependency}")[:success?]
       log "#{dependency} already exists. Skipping..."
     else
       if shell("brew install #{dependency}")
+        log "Successfully installed #{dependency}."
+      else
+        log "There was a problem installing #{dependency}."
+        exit 1
+      end
+    end
+  end
+end
+
+def install_cask_dependencies
+  log "Installing dependencies from homebrew-cask..."
+  cask_dependencies.each do |dependency|
+    log "Installing #{dependency}..."
+    if shell("brew cask list #{dependency}")[:success?]
+      log "#{dependency} already exists. Skipping..."
+    else
+      if shell("brew cask install #{dependency}")
         log "Successfully installed #{dependency}."
       else
         log "There was a problem installing #{dependency}."
@@ -99,6 +116,10 @@ end
 
 def dependencies
   %w(git nvim vim zsh tmux gpg fzf the_silver_searcher)
+end
+
+def cask_dependencies
+  %w(iterm2 ubersicht slate)
 end
 
 def dotfiles

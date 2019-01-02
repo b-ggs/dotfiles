044c0bf44ce45543fe499863c530ebafafa462e6
boggs <hello@boggs.xyz>
2019-01-02 14:19:12 +0000

    fix(zsh): Load binds after zgen because something somewhere is overwriting it

--- zsh/init.zsh
+++ zsh/init.zsh
@@ -7,8 +7,8 @@
 #  ██████ ██████ ░██  ░██░███   ░░█████
 # ░░░░░░ ░░░░░░  ░░   ░░ ░░░     ░░░░░
 
-source $HOME/.zsh/base.zsh
 source $HOME/.zsh/_zgen.zsh
+source $HOME/.zsh/base.zsh
 source $HOME/.zsh/_chruby.zsh
 source $HOME/.zsh/_env.zsh
 source $HOME/.zsh/_aliases.zsh

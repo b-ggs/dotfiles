d4424908b3988084eba04edb9e0d3ac74ac541e1
boggs <hello@boggs.xyz>
2019-01-03 18:44:17 +0800

    chore(zsh): Move base.zsh to _binds.zsh

--- zsh/init.zsh
+++ zsh/init.zsh
@@ -8,7 +8,7 @@
 # ░░░░░░ ░░░░░░  ░░   ░░ ░░░     ░░░░░
 
 source $HOME/.zsh/_zgen.zsh
-source $HOME/.zsh/base.zsh
+source $HOME/.zsh/_binds.zsh
 source $HOME/.zsh/_chruby.zsh
 source $HOME/.zsh/_env.zsh
 source $HOME/.zsh/_aliases.zsh

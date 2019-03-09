cc21a73bb863612e012ac340849325a91113471d
boggs <hello@boggs.xyz>
2019-03-09 22:38:18 +0800

    feat(zsh): Remove chruby

--- zsh/_chruby.zsh
+++
@@ -1,5 +0,0 @@
-CHRUBY="/usr/local/opt/chruby/share/chruby/chruby.sh"
-CHRUBY_AUTO="/usr/local/opt/chruby/share/chruby/auto.sh"
-
-[[ -e "$CHRUBY" ]] && source $CHRUBY
-[[ -e "$CHRUBY_AUTO" ]] && source $CHRUBY_AUTO
--- zsh/init.zsh
+++ zsh/init.zsh
@@ -9,7 +9,6 @@
 
 source $HOME/.zsh/_zgen.zsh
 source $HOME/.zsh/_binds.zsh
-source $HOME/.zsh/_chruby.zsh
 source $HOME/.zsh/_env.zsh
 source $HOME/.zsh/_aliases.zsh
 source $HOME/.zsh/_fzf.zsh

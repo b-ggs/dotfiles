31e5cfef315e873f405140135eae129a08d9e134
boggs <hello@boggs.xyz>
2019-11-29 02:45:33 +0000

    Add docker and docker-compose completions

--- zsh/_zgen.zsh
+++ zsh/_zgen.zsh
@@ -2,5 +2,7 @@ source "$HOME/.zgen/zgen.zsh"
 if [[ -n "$(command -v zgen 2> /dev/null)" ]] && ! zgen saved; then
   zgen oh-my-zsh
   zgen oh-my-zsh plugins/git
+  zgen oh-my-zsh plugins/docker
+  zgen oh-my-zsh plugins/docker-compose
   zgen save
 fi

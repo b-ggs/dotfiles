55a216792cb26be9e824d75ad2efbb429cee9b8c
boggs <hello@boggs.xyz>
2020-08-26 02:25:35 +0800

    Add time to zsh prompt

--- zsh/_prompt.zsh
+++ zsh/_prompt.zsh
@@ -3,7 +3,8 @@ local USER_HOST="%K{9} %F{0}%n%f%F{0}@%F{0}%m%f %k "
 local PWD_PROMPT="%K{12} %F{0}%~%f %k"
 ZSH_THEME_GIT_PROMPT_PREFIX=" %K{12} %F{0}git@"
 ZSH_THEME_GIT_PROMPT_SUFFIX="%f %k"
-local GIT_PROMPT='$(git_prompt_info)'
+local GIT_PROMPT='$(git_prompt_info) '
+local TIME="%K{12} %F{0}%*%f %k"
 local RETURN_PROMPT="%(?.. %K{12} %F{0}%?%f %k)"
 
-PROMPT="${USER_HOST}${SSH_CLIENT_PROMPT}${PWD_PROMPT}${GIT_PROMPT}${RETURN_PROMPT}"$'\n'"$ "
+PROMPT="${USER_HOST}${SSH_CLIENT_PROMPT}${PWD_PROMPT}${GIT_PROMPT}${TIME}${RETURN_PROMPT}"$'\n'"$ "

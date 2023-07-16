d81f0cc3922da87903d273996f87aa1804853f4d
boggs <6130147+b-ggs@users.noreply.github.com>
2023-07-17 02:48:43 +0800

    Replace stylua with luaformatter, rustfmt is deprecated

--- nvim/.config/nvim/lua/plugins/null-ls.lua
+++ nvim/.config/nvim/lua/plugins/null-ls.lua
@@ -9,7 +9,6 @@ return {
 				null_ls.builtins.formatting.djhtml,
 				null_ls.builtins.formatting.prettier,
 				null_ls.builtins.formatting.ruff,
-				null_ls.builtins.formatting.rustfmt,
 				null_ls.builtins.formatting.stylua,
 			},
 		}

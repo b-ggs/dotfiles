bd9650f8b4a0b3b8f3ee488d819932015e746692
boggs <6130147+b-ggs@users.noreply.github.com>
2023-07-22 02:32:50 +0800

    Fix nvim-treesitter config

--- nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -5,29 +5,28 @@ return {
     "JoosepAlviste/nvim-ts-context-commentstring",
     "p00f/nvim-ts-rainbow",
   },
-  opts = {
-    ensure_installed = {
-      "astro",
-      "dockerfile",
-      "hcl",
-      "help",
-      "html",
-      "javascript",
-      "json",
-      "lua",
-      "markdown",
-      "python",
-      "query",
-      "rust",
-      "svelte",
-      "typescript",
-      "vim",
-    },
-    -- highlight = {
-    -- 	enable = true,
-    -- 	additional_vim_regex_highlighting = false,
-    -- },
-    rainbow = { enable = true, extended_mode = true },
-    context_commentstring = { enable = true, enable_autocmd = false },
-  },
+  config = function()
+    local configs = require("nvim-treesitter.configs")
+    configs.setup({
+      ensure_installed = {
+        "astro",
+        "dockerfile",
+        "hcl",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "python",
+        "query",
+        "rust",
+        "svelte",
+        "typescript",
+        "vim",
+      },
+      highlight = { enable = true },
+      rainbow = { enable = true, extended_mode = true },
+      context_commentstring = { enable = true, enable_autocmd = false },
+    })
+  end,
 }

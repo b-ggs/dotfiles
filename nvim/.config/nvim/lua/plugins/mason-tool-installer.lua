3fbee13d4c61d0c2ca76bd67518954f557dd1560
boggs <6130147+b-ggs@users.noreply.github.com>
2023-07-22 00:44:01 +0800

    Add initial stylua config, add failing stylua tests

--- nvim/.config/nvim/lua/plugins/mason-tool-installer.lua
+++ nvim/.config/nvim/lua/plugins/mason-tool-installer.lua
@@ -1,12 +1,23 @@
 return {
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-    opts = {
-        ensure_installed = {
-            "astro-language-server", "black", "css-lsp", "emmet-ls", "html-lsp",
-            "lua-language-server", "prettier", "pyright", "ruff", "stylua",
-            "svelte-language-server", "terraform-ls", "tflint",
-            "typescript-language-server", "vim-language-server"
-        },
-        run_on_start = false
-    }
+  "WhoIsSethDaniel/mason-tool-installer.nvim",
+  opts = {
+    ensure_installed = {
+      "astro-language-server",
+      "black",
+      "css-lsp",
+      "emmet-ls",
+      "html-lsp",
+      "lua-language-server",
+      "prettier",
+      "pyright",
+      "ruff",
+      "stylua",
+      "svelte-language-server",
+      "terraform-ls",
+      "tflint",
+      "typescript-language-server",
+      "vim-language-server",
+    },
+    run_on_start = false,
+  },
 }

518934acf63022b65dbda992dbb841c46d776124
boggs <6130147+b-ggs@users.noreply.github.com>
2023-08-05 00:54:58 +0800

    Add nvim-spectre

---
+++ nvim/.config/nvim/lua/plugins/nvim-spectre.lua
@@ -0,0 +1,11 @@
+return {
+  "nvim-pack/nvim-spectre",
+  opts = {
+    color_devicons = false,
+  },
+  init = function()
+    vim.keymap.set('n', '<leader>a', '<cmd>lua require("spectre").toggle()<CR>', {
+        desc = "Toggle Spectre"
+    })
+  end,
+}

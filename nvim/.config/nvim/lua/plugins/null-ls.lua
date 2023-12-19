f3a22879156ccd58f7cbc486c9bdecd33c653fc1
boggs <6130147+b-ggs@users.noreply.github.com>
2023-12-19 16:39:31 +0800

    Remove black and djhtml

--- nvim/.config/nvim/lua/plugins/null-ls.lua
+++ nvim/.config/nvim/lua/plugins/null-ls.lua
@@ -5,8 +5,6 @@ return {
 
     return {
       sources = {
-        null_ls.builtins.formatting.black,
-        null_ls.builtins.formatting.djhtml,
         null_ls.builtins.formatting.prettier,
         null_ls.builtins.formatting.ruff,
         null_ls.builtins.formatting.ruff_format,

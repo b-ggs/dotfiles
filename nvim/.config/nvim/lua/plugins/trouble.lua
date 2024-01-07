3d1e9962a80997ccf961044abe2d09899d60353c
boggs <6130147+b-ggs@users.noreply.github.com>
2024-01-08 03:17:00 +0800

    Add trouble

---
+++ nvim/.config/nvim/lua/plugins/trouble.lua
@@ -0,0 +1,15 @@
+return {
+  "folke/trouble.nvim",
+  opts = {
+    fold_open = "▼",
+    fold_closed = "▶",
+    icons = false,
+    signs = {
+      error = "E",
+      warning = "W",
+      hint = "H",
+      information = "I",
+      other = "O",
+    },
+  },
+}

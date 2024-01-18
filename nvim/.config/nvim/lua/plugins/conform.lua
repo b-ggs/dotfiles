37c9315ee41a066fdef8f8fa9bf1a9098848afcd
boggs <6130147+b-ggs@users.noreply.github.com>
2024-01-18 17:03:27 +0800

    Update conform config

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -1,16 +1,27 @@
 return {
   "stevearc/conform.nvim",
   tag = "stable",
+  event = { "BufWritePre" },
+  cmd = { "ConformInfo" },
+  keys = {
+    {
+      "gf",
+      function()
+        require("conform").format({ async = true, lsp_fallback = true })
+      end,
+      mode = "",
+      desc = "Format buffer",
+    },
+  },
   opts = {
     formatters_by_ft = {
+      css = { "prettier" },
       javascript = { "prettier" },
+      html = { "prettier" },
       lua = { "stylua" },
       markdown = { "prettier" },
       python = { "ruff_fix", "ruff_format" },
       ["*"] = { "trim_whitespace" },
     },
   },
-  init = function()
-    vim.keymap.set("n", "gf", ":lua require('conform').format()<CR>")
-  end,
 }

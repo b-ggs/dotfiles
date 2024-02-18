741c528ffa3d6fefe8014b73f214bf4a2c47fb78
boggs <6130147+b-ggs@users.noreply.github.com>
2024-02-18 18:33:25 +0800

    local

--- nvim/.config/nvim/lua/plugins/alpha.lua
+++ nvim/.config/nvim/lua/plugins/alpha.lua
@@ -2,7 +2,7 @@ return {
   "goolord/alpha-nvim",
   event = "VimEnter",
   opts = function()
-    startify = require("alpha.themes.startify")
+    local startify = require("alpha.themes.startify")
     startify.config.layout[2].val = "welcome to hell"
     startify.nvim_web_devicons.enabled = false
     return startify.config

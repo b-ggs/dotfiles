61ceee50114d8fb525063ee309bc78d55704f99c
boggs <6130147+b-ggs@users.noreply.github.com>
2024-03-18 13:31:16 +0800

    Remove BKK stuff bc it's making me anxious

--- swiftbar/thailand.3h.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-from datetime import datetime
-
-target_date = datetime(2024, 3, 21)
-current_date = datetime.now()
-days_remaining = (target_date - current_date).days
-
-print(f"BKK in {days_remaining} days")

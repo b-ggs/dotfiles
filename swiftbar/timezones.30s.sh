32e32b2e8b5049a710d0c30943815542c7e38679
boggs <6130147+b-ggs@users.noreply.github.com>
2024-03-20 00:39:10 +0800

    Add MNL to list

--- swiftbar/timezones.30s.sh
+++ swiftbar/timezones.30s.sh
@@ -4,6 +4,7 @@ echo -n "VNO " ; TZ="Europe/Vilnius" date +'%a %-l:%M %p'
 
 echo "---"
 
+echo -n "MNL " ; TZ="Asia/Manila" date +'%a %Y-%m-%d %-l:%M %p'
 echo -n "VNO " ; TZ="Europe/Vilnius" date +'%a %Y-%m-%d %-l:%M %p'
 echo -n "LAX " ; TZ="America/Los_Angeles" date +'%a %Y-%m-%d %-l:%M %p'
 echo -n "DEN " ; TZ="America/Denver" date +'%a %Y-%m-%d %-l:%M %p'

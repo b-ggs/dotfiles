044324dd87ae5af0f831f8bd3d807b72e65c8d2b
boggs 🌊 <6130147+b-ggs@users.noreply.github.com>
2026-06-24 00:43:26 +0800

    Idk why this is back

--- nvim/.config/nvim/lua/plugins/comment.lua
+++ nvim/.config/nvim/lua/plugins/comment.lua
@@ -1,8 +1,8 @@
 return {
   "numToStr/Comment.nvim",
-  -- dependencies = {
-  --   "JoosepAlviste/nvim-ts-context-commentstring",
-  -- },
+  dependencies = {
+    "JoosepAlviste/nvim-ts-context-commentstring",
+  },
   opts = function()
     return {
       pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
